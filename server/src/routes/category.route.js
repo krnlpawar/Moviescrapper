3289df6d297432d3f1bc6b22d98c9d3c90006a17
krnlpawar <krnlpawar@gmail.com>
2025-01-20 22:00:37 +0530

    workiing on formating

--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -4,14 +4,32 @@ import categoryController from "../controllers/category.controller.js";
 
 const router = Router();
 
-router.route("/categories").get(isAuthenticated, categoryController.getAllCategories);
-router.route("/category/store").post(isAuthenticated, categoryController.storeCategory);
-router.route("/category/:id/update").patch(isAuthenticated, categoryController.updateCategory);
-router.route("/category/:id/delete").post(isAuthenticated, categoryController.deleteCategory);
-router.route("/category/:id/restore").post(isAuthenticated, categoryController.restoreCategory);
-router.route("/category/:id/pin").get(isAuthenticated, categoryController.pinCategory);
-router.route("/category/:id/unpin").get(isAuthenticated, categoryController.unpinCategory);
-router.route("/category/:id/order").get(isAuthenticated, categoryController.orderCategory);
-router.route("/category/search").get(isAuthenticated, categoryController.searchCategory);
+router
+  .route("/categories")
+  .get(isAuthenticated, categoryController.getAllCategories);
+router
+  .route("/category/store")
+  .post(isAuthenticated, categoryController.storeCategory);
+router
+  .route("/category/:id/update")
+  .patch(isAuthenticated, categoryController.updateCategory);
+router
+  .route("/category/:id/delete")
+  .post(isAuthenticated, categoryController.deleteCategory);
+router
+  .route("/category/:id/restore")
+  .post(isAuthenticated, categoryController.restoreCategory);
+router
+  .route("/category/:id/pin")
+  .get(isAuthenticated, categoryController.pinCategory);
+router
+  .route("/category/:id/unpin")
+  .get(isAuthenticated, categoryController.unpinCategory);
+router
+  .route("/category/:id/order")
+  .get(isAuthenticated, categoryController.orderCategory);
+router
+  .route("/category/search")
+  .get(isAuthenticated, categoryController.searchCategory);
 
 export default router;
